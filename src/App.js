5cd688120d96490e4ad3b332e1acbc277d14d9a8
Dmytro Danyliuk <dmytro.danyliuk@mindgeek.com>
2018-07-19 16:54:53 -0400

    Added Header and menu

--- src/App.js
+++ src/App.js
@@ -1,21 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import MenuHeader from "./components/Menu";
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import deepPurple from '@material-ui/core/colors/deepPurple';
+import lightBlue from '@material-ui/core/colors/lightBlue';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: deepPurple,
+    secondary: lightBlue,
+  },
+});
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  flex: {
+    flexGrow: 1,
+    textAlign: "center"
+  }
+};
 
 class App extends Component {
   render() {
+    const {classes} = this.props;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <div className={classes.root}>
+          <AppBar position="static">
+            <Toolbar>
+              <MenuHeader/>
+              <Typography variant="title" color="inherit" className={classes.flex}>
+                News
+              </Typography>
+              <Button color="inherit">Login</Button>
+            </Toolbar>
+          </AppBar>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
 
-export default App;
+
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(App);
+
