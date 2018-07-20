077dcc58ce2b8e1681ad14d958762c0e0a46afe4
Dmytro Danyliuk <dmytro.danyliuk@mindgeek.com>
2018-07-20 17:18:40 -0400

    Add left drawer action for header button

--- src/App.js
+++ src/App.js
@@ -6,10 +6,15 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import MenuHeader from "./components/Menu";
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import deepPurple from '@material-ui/core/colors/deepPurple';
 import lightBlue from '@material-ui/core/colors/lightBlue';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import 'typeface-roboto'
 
 const theme = createMuiTheme({
   palette: {
@@ -24,25 +29,91 @@ const styles = {
   },
   flex: {
     flexGrow: 1,
-    textAlign: "center"
-  }
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: 'auto',
+  },
 };
 
 class App extends Component {
+
+  state = {
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  };
+
+  toggleDrawer = (side, open) => () => {
+    this.setState({
+      [side]: open,
+    });
+  };
+
   render() {
     const {classes} = this.props;
+
+    const sideList = (
+      <div className={classes.list}>
+        <List>Item1</List>
+        <Divider/>
+        <List>Item2</List>
+        <Divider/>
+        <List>Item3</List>
+        <Divider/>
+        <List>Item4</List>
+        <Divider/>
+        <List>Item5</List>
+        <Divider/>
+      </div>
+    );
+
+    const fullList = (
+      <div className={classes.fullList}>
+        <List>Item1</List>
+        <Divider/>
+        <List>Item2</List>
+        <Divider/>
+        <List>Item3</List>
+        <Divider/>
+        <List>Item4</List>
+        <Divider/>
+        <List>Item5</List>
+        <Divider/>
+      </div>
+    );
+
     return (
       <MuiThemeProvider theme={theme}>
         <div className={classes.root}>
           <AppBar position="static">
             <Toolbar>
-              <MenuHeader/>
+              <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+                <MenuIcon onClick={this.toggleDrawer('left', true)} />
+              </IconButton>
               <Typography variant="title" color="inherit" className={classes.flex}>
                 News
               </Typography>
               <Button color="inherit">Login</Button>
             </Toolbar>
           </AppBar>
+          <Drawer open={this.state.left} onClose={this.toggleDrawer('left', false)}>
+            <div
+              tabIndex={0}
+              role="button"
+              onClick={this.toggleDrawer('left', false)}
+              onKeyDown={this.toggleDrawer('left', false)}
+            >
+              {sideList}
+            </div>
+          </Drawer>
         </div>
       </MuiThemeProvider>
     );
